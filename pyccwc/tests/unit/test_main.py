d1c695fe4b6c36f516e8b62991d00e7a6cc16ac2
Sebastian Rodriguez Colina <srcolinas@gmail.com>
2023-12-16 16:44:25 -0500

    lines - split counts from formating

--- pyccwc/tests/unit/test_main.py
+++ pyccwc/tests/unit/test_main.py
@@ -1,11 +1,10 @@
-from pathlib import Path
-
 import pytest
 
-from pyccwc.main import count, format
+from pyccwc.main import count
 
 
-@pytest.mark.parametrize("content,num_lines", [("A \n piece \n of text.", 3), ("another piece \n", 2)])
+@pytest.mark.parametrize(
+    "content,num_lines", [("A \n piece \n of text.", 3), ("another piece \n", 1)]
+)
 def test_number_of_lines(content, num_lines):
     assert count(content, count_lines=True) == num_lines
-
