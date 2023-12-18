d02b7fc4306f605f7449362b9f0161786860abcc
Sebastian Rodriguez Colina <srcolinas@gmail.com>
2023-12-18 11:05:59 -0500

    finish all

--- pyccwc/pyccwc/main.py
+++ pyccwc/pyccwc/main.py
@@ -1,26 +1,81 @@
-from pathlib import Path
+import string
+from typing import NamedTuple, TextIO
 
 
-def count(content: str, *, count_lines: bool) -> int:
-    result = -1
-    if count_lines:
-        result = len(content.splitlines())
+class Counts(NamedTuple):
+    lines: int
+    words: int
+    characters: int
+    bytes: int
+
+
+def count(
+    file: TextIO,
+    /,
+    count_lines: bool = False,
+    count_bytes: bool = False,
+    count_words: bool = False,
+    count_characters: bool = False,
+) -> Counts:
+    default = (
+        not count_lines and not count_bytes and not count_words and not count_characters
+    )
+    num_bytes = 0 if count_bytes or default else -1
+    num_words = 0 if count_words or default else -1
+    num_characters = 0 if count_characters else -1
+    i = -1
+    for i, line in enumerate(file):
+        if count_bytes or default:
+            num_bytes += len(line.encode())
+            num_bytes += 1
+        if count_words or default:
+            num_words += len(line.split())
+        if count_characters:
+            old = line
+            for w in string.whitespace:
+                new = old.replace(w, "")
+                old = new
+            num_characters += len(new)
+
+    num_lines = -1 if not count_lines and not default else i + 1
+    return Counts(num_lines, num_words, num_characters, num_bytes)
+
+
+def format(filename: str, counts: Counts) -> str:
+    result = ""
+    for c in counts:
+        if c != -1:
+            result += f"{c} "
+    result += f"{filename}"
     return result
-
-
-def format(file: Path, count: int) -> str:
-    return f"{count} {file}"
 
 
 def _cli():
     import argparse
+    import sys
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("file", type=Path)
+    parser.add_argument(
+        "file", nargs="?", type=argparse.FileType("r"), default=sys.stdin
+    )
     parser.add_argument("-l", "--lines", action="store_true")
+    parser.add_argument("-c", "--bytes", action="store_true")
+    parser.add_argument("-w", "--words", action="store_true")
+    parser.add_argument("-m", "--chars", action="store_true")
 
     args = parser.parse_args()
-    print(format(args.file, count(args.file.read_text(), count_lines=args.lines)))
+    print(
+        format(
+            args.file.name if args.file.name != "<stdin>" else "",
+            count(
+                args.file,
+                count_lines=args.lines,
+                count_bytes=args.bytes,
+                count_words=args.words,
+                count_characters=args.chars,
+            ),
+        )
+    )
 
 
 if __name__ == "__main__":
--- pyccwc/tests/unit/test_main.py
+++ pyccwc/tests/unit/test_main.py
@@ -1,8 +1,8 @@
-from pathlib import Path
+import io
 
 import pytest
 
-from pyccwc.main import count, format
+from pyccwc.main import count, format, Counts
 
 
 @pytest.mark.parametrize(
@@ -10,8 +10,80 @@ from pyccwc.main import count, format
     [("A \n piece \n of text.", 3), ("another piece \n", 1), ("", 0)],
 )
 def test_number_of_lines(content, num_lines):
-    assert count(content, count_lines=True) == num_lines
+    assert count(io.StringIO(content), count_lines=True) == (num_lines, -1, -1, -1)
 
 
-def test_formating():
-    assert format(Path("name.txt"), 3) == "3 name.txt"
+@pytest.mark.parametrize(
+    "content,num_bytes",
+    [("aa\x01\x02b桜", 9), ("", 0)],
+)
+def test_number_of_bytes(content, num_bytes):
+    assert count(io.StringIO(content), count_bytes=True) == (-1, -1, -1, num_bytes)
+
+
+@pytest.mark.parametrize(
+    "content,num_words",
+    [("hello to\teveryone\x0cin\rthe\x0bworld", 6), ("", 0)],
+)
+def test_number_of_words(content, num_words):
+    assert count(io.StringIO(content), count_words=True) == (-1, num_words, -1, -1)
+
+
+@pytest.mark.parametrize(
+    "content,num_characters",
+    [("ab桜", 3), ("", 0)],
+)
+def test_number_of_characters(content, num_characters):
+    assert count(io.StringIO(content), count_characters=True) == (
+        -1,
+        -1,
+        num_characters,
+        -1,
+    )
+
+
+## Note that we do not need to be very exhaustive with
+## the following test, edge cases are to be covered in the
+## previous ones.
+@pytest.mark.parametrize(
+    "content,result",
+    [("Dear programmer\n please write tests", (2, 5, 30, 37)), ("", (0, 0, 0, 0))],
+)
+def test_combined_calls(content, result):
+    assert (
+        count(
+            io.StringIO(content),
+            count_lines=True,
+            count_bytes=True,
+            count_words=True,
+            count_characters=True,
+        )
+        == result
+    )
+
+
+## Note that we do not need to be very exhaustive with
+## the following test, edge cases are to be covered in the
+## previous ones.
+@pytest.mark.parametrize(
+    "content,result",
+    [("Dear programmer\n please write tests", (2, 5, -1, 37)), ("", (0, 0, -1, 0))],
+)
+def test_default(content, result):
+    assert count(io.StringIO(content)) == result
+
+
+@pytest.mark.parametrize(
+    "counts,result_numbers",
+    [
+        (Counts(5, -1, -1, -1), "5"),
+        (Counts(-1, 3, -1, -1), "3"),
+        (Counts(-1, -1, 6, -1), "6"),
+        (Counts(-1, -1, -1, 9), "9"),
+        (Counts(4, 2, -1, -1), "4 2"),
+        (Counts(4, 2, 7, -1), "4 2 7"),
+        (Counts(4, 2, 7, 8), "4 2 7 8"),
+    ],
+)
+def test_formating(counts, result_numbers):
+    assert format("name.txt", counts) == f"{result_numbers} name.txt"
